e7c08a6571f81bad60619e4137aa7b6c597e90a9
Laura <lmcsay@gmail.com>
2020-05-31 12:09:58 -0500

    connect to database

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
+
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,6 +22,7 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
 
@@ -38,6 +41,14 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    # genres = (list of strings)
+    # website = db.Column(db.String(120))
+    # seeking_talent = db.Column(db.Boolean)
+    # seeking_description = db.Column(db.String(120))
+    # past_shows = dictionary(artist id, artist name, artist_image_link, start_time)
+    # upcoming_shows = dictionary(artist id, artist name, artist_image_link, start_time)
+    # past_shows_count = db.Column(db.Integer)
+    # upcoming_shows_count = db.Column(db.Integer)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 SECRET_KEY = os.urandom(32)
-# Grabs the folder where the script runs.
+# Grabs the folder where the script runs (path to the current directory)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
@@ -8,6 +8,6 @@ DEBUG = True
 
 # Connect to the database
 
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyur'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
