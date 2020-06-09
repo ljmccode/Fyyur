4c98979b3bfc81ab8ac7da62c8f04c887b178e58
Laura <lmcsay@gmail.com>
2020-06-08 21:13:37 -0500

    artist search

--- app.py
+++ app.py
@@ -134,8 +134,6 @@ def create_venue_form():
 
 @ app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
     error=False
     data=request.form
 
@@ -181,35 +179,27 @@ def delete_venue(venue_id):
 @ app.route('/artists')
 def artists():
     artists = Artist.query.all()
-    
     data = []
     
     for artist in artists:
         data.append(artist)
-
     return render_template('pages/artists.html', artists=data)
 
 
 @ app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
-    response={
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
-    }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    search_term = request.form.get('search_term', '')
+    results =  Artist.query.order_by(Artist.id).filter(Artist.name.ilike('%{}%'.format(search_term))).all()
+    
+    response = {}
+    response['count'] = len(results)
+    response['data'] = results
+   
+    return render_template('pages/search_artists.html', results=response, search_term=search_term)
 
 
 @ app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
-    # shows the venue page with the given venue_id
-    # TODO: replace with real venue data from the venues table, using venue_id
     artist = Artist.query.get(artist_id)
     data = artist.dictionary()
 
@@ -298,9 +288,6 @@ def create_artist_form():
 
 @ app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-    # called upon submitting the new artist listing form
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
     error=False
     data=request.form
 
