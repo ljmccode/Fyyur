d44ba5b69f7c735424e00e93cd298dababad9ee1
Laura <lmcsay@gmail.com>
2020-06-08 23:12:24 -0500

    edit venue

--- app.py
+++ app.py
@@ -257,28 +257,38 @@ def edit_artist_submission(artist_id):
 @ app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
     form=VenueForm()
-    venue={
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    }
-    # TODO: populate form with values from venue with ID <venue_id>
+    venue = Venue.query.get(venue_id).dictionary()
+  
     return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 
 @ app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-    # TODO: take values from the form submitted, and update existing
-    # venue record with ID <venue_id> using the new attributes
+    error = False
+    data=request.form
+    venue = Venue.query.filter(Venue.id == venue_id).one_or_none()
+
+    try:
+        venue.name=data.get('name'),
+        venue.city=data.get('city'),
+        venue.state=data.get('state'),
+        venue.address=data.get('address'),
+        venue.phone=data.get('phone'),
+        venue.image_link=data.get('image_link'),
+        venue.facebook_link=data.get('facebook_link'),
+        venue.genres=data.getlist('genres'),
+        venue.website=data.get('website'),
+        
+        db.session.commit()
+        # on successful db insert, flash success
+        flash('Venue ' + data['name'] + ' was successfully updated!')
+    except():
+        db.session.rollback()
+        error=True
+        # on unsuccessful db insert, flash an error
+        flash('An error occurred. Venue' +
+              data['name'] + ' could not be updated.')
+
     return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
